e16197913b5228c8fdd1cbf2881b3fe8eed6ebc8
LANGJIALINGBOJUE <1830879596@qq.com>
2022-09-07 17:19:17 +0800

    commit提交测试 2022年9月7日

---
+++ src/test/java/com/langjialing/helloworld/Test.java
@@ -0,0 +1,13 @@
+package com.langjialing.helloworld;
+
+public class Test {
+    public static void main(String[] args) {
+        String date = "2017-07-17 11:03:52";
+        System.out.println("compareToBefore1 : "+date.compareTo("2017-07-16"));
+//        System.out.println("compareToBefore2 : "+date.compareTo("2017-05-16 11:03:52"));
+//        System.out.println("compareToNow1 : "+date.compareTo("2017-07-17 11:03:52"));
+//        System.out.println("compareToNow2 : "+date.compareTo("2017-07-17"));
+//        System.out.println("compareToAfter1 : "+date.compareTo("2017-07-18 11:03:52"));
+//        System.out.println("compareToAfter2 : "+date.compareTo("2017-09-16 11:03:52"));
+    }
+}
