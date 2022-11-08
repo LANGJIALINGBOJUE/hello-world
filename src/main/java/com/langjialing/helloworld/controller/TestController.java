6c7ca16bbbf90bafcd68648f1689273165324ab0
LANGJIALINGBOJUE <1830879596@qq.com>
2022-11-08 16:43:18 +0800

    提交代码

--- src/main/java/com/langjialing/helloworld/controller/TestController.java
+++ src/main/java/com/langjialing/helloworld/controller/TestController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.Serializable;
+
+/**
+ * @author 郎家岭伯爵
+ */
 @Controller
 public class TestController {
 
