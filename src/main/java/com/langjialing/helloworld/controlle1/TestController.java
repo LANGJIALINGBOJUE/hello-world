45118b845e6acfe6159e0f813dbb2c2609116ed6
LANGJIALINGBOJUE <1830879596@qq.com>
2023-09-10 22:35:32 +0800

    更新代码

---
+++ src/main/java/com/langjialing/helloworld/controlle1/TestController.java
@@ -0,0 +1,10 @@
+package com.langjialing.helloworld.controlle1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liyuwei
+ */
+@RestController
+public class TestController {
+}
