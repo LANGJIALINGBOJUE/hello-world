316f3c9d6de0b5ed27668d17edcc0d60b0c571d7
LANGJIALINGBOJUE <1830879596@qq.com>
2024-01-02 10:25:32 +0800

    更新代码

--- src/test/java/com/langjialing/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/langjialing/helloworld/HelloWorldApplicationTests.java
@@ -1,8 +1,16 @@
 package com.langjialing.helloworld;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
@@ -10,9 +18,50 @@ class HelloWorldApplicationTests {
     @Value("${spring.redis.host}")
     private String redisHost;
 
+    private final static String QUEUE_NAME = "hello";
+
     @Test
     void test1(){
         System.out.println(redisHost);
+    }
+
+    /**
+     * rabbitmq provider
+     */
+    @Test
+    void test2(){
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.91.128");
+        try ( Connection connection = factory.newConnection();
+              Channel channel = connection.createChannel();
+        ){
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * rabbitmq consumer
+     */
+    @Test
+    void test3() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.91.128");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
 
 }
