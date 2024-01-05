8e4a4ef44d583660980d601a1755d86457eae9dd
LANGJIALINGBOJUE <1830879596@qq.com>
2024-01-05 11:29:00 +0800

    更新代码

--- HelloWorld1/src/main/java/com/langjialing/helloworld1/HelloWorld1Application.java
+++ HelloWorld1/src/main/java/com/langjialing/helloworld1/HelloWorld1Application.java
@@ -1,13 +1,21 @@
 package com.langjialing.helloworld1;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloWorld1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld1Application.class, args);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 
 }
--- HelloWorld1/src/test/java/com/langjialing/helloworld1/HelloWorld1ApplicationTests.java
+++ HelloWorld1/src/test/java/com/langjialing/helloworld1/HelloWorld1ApplicationTests.java
@@ -1,9 +1,18 @@
 package com.langjialing.helloworld1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class HelloWorld1ApplicationTests {
@@ -46,4 +55,30 @@ class HelloWorld1ApplicationTests {
         rabbitTemplate.convertAndSend(exchangeName, "china.weather", message);
     }
 
+    @Test
+    public void testObjectRabbitmqTopic() {
+        // 指定交换机名称
+        String exchangeName = "langjialing.topic";
+
+        Map<String, Object> map = new HashMap<>(2);
+        map.put("name", "小明");
+        map.put("age", 21);
+
+        System.out.println(map);
+        rabbitTemplate.convertAndSend(exchangeName, "china.weather", map);
+    }
+
+    @Test
+    void testPageOut(){
+        Message message = MessageBuilder
+                .withBody("hello".getBytes(StandardCharsets.UTF_8))
+                .setDeliveryMode(MessageDeliveryMode.PERSISTENT).build();
+        for (int i = 0; i < 1000000; i++) {
+//            rabbitTemplate.convertAndSend("langjialing.topic", "china.sport", message);
+            rabbitTemplate.convertAndSend("topic.queue1",  message);
+
+        }
+
+        List<Integer> list = new ArrayList<>();
+    }
 }
